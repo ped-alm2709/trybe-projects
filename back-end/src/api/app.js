2b53358513cd6a3a9a80c3044fb03f47eda9ea7b
Rodrigo <rodrigopython16@gmail.com>
2022-01-03 15:20:45 -0300

    Initializing

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+app.get("/", (_req, res) => res.status(200));
 
 module.exports = app;
