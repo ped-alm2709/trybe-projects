082ab9d725b98285e3d4e7f6cbe3c3b869c3b536
Rodrigo <rodrigopython16@gmail.com>
2022-01-03 19:18:01 -0300

    login done

--- back-end/src/api/controllers/userController.js
+++ back-end/src/api/controllers/userController.js
@@ -4,7 +4,7 @@ const login = async (req, res) => {
   try {
     const response = await userService.login(req.body);
 
-    res.status(200).json({ token: response });
+    res.status(201).json({ token: response });
   } catch (error) {
     res.status(404).json({ error: error.message });
   }
