bd2cd41df6c53b0fe4f3251ae4cf4fcc0c48fbb0
Leandro Lopes <leandro_carlos@uol.com.br>
2022-01-04 11:11:44 -0300

    requisito 06

--- front-end/src/context/Provider.js
+++ front-end/src/context/Provider.js
@@ -1,25 +1,30 @@
-import React, { useState} from "react";
-import Context from "./Context";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import ContextRegister from './ContextRegister';
 
 function Provider({ children }) {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-const state = {
+  const state = {
     name,
     email,
     password,
     setName,
     setEmail,
     setPassword,
-};
+  };
 
-return (
-    <Context.Provider value={state}>
-        {children}
-    </Context.Provider>
-);
+  return (
+    <ContextRegister.Provider value={ state }>
+      {children}
+    </ContextRegister.Provider>
+  );
 }
+
+Provider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 
 export default Provider;
--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,85 +1,92 @@
-import React, { useState, useEffect } from "react";
-import Context from "../context/Context";
-import { useHistory } from "react-router-dom";
+import React, { useState, useEffect, useContext } from 'react';
+// import { useHistory } from 'react-router-dom';
+import ContextRegister from '../context/ContextRegister';
 
 function Register() {
-    const [isDisabled, setIsDisabled] = useState(true);
-    const [errorMsg, setErrorMsg] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [errorMsg, setErrorMsg] = useState(false);
 
-    const { name, email, password, setName, setEmail, setPassword } = useContext(Context);
-    
-    const history = useHistory();
+  const {
+    name,
+    email,
+    password,
+    setName,
+    setEmail,
+    setPassword } = useContext(ContextRegister);
 
-useEffect(() => {
+  // const history = useHistory();
+
+  useEffect(() => {
     const isValid = () => {
-        const emailValid = email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
-        const min = 6;
-        const max = 12;
-        const nameValid = name.length > max;
-        const passwordValid = password.length >= min;
+      const emailValid = email.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+      const min = 6;
+      const max = 12;
+      const nameValid = name.length > max;
+      const passwordValid = password.length >= min;
 
-        if (emailValid && nameValid) {
-            if(passwordValid) {
-                setIsDisabled(false);
-            }
-        } else {
-            setIsDisabled(true);
+      if (emailValid && nameValid) {
+        if (passwordValid) {
+          setIsDisabled(false);
         }
+      } else {
+        setIsDisabled(true);
+      }
     };
     isValid();
-}, [email, name, password, isDisabled]);
+  }, [email, name, password, isDisabled]);
 
-const setToken = (token) => {
-    localStorage.setItem("token", JSON.stringify(token));
-    history.push({ patchname: '/customer/products'});
-};
-
-const handleSubmitRegister = async () => {
+  /* const setToken = (token) => {
+    localStorage.setItem('token', JSON.stringify(token));
+    history.push({ patchname: '/customer/products' });
+  };
+  */
+  const handleSubmitRegister = async () => {
     const create = await createUser({ name, email, password });
     if (!create) return setErrorMsg(true);
     setToken(create.token);
     return create;
-};
-
+  };
 
   return (
     <div>
       <h1>Cadastro</h1>
-        <form>
-            <label>
-                Nome:
-                <input type="text" 
-                name="nome" 
-                data-testid="common_register__input-name" 
-                onChange={ ( { e }) => setName(e.target.value) }
-                />
-            </label>
-            <label>
-                Email:
-                <input type="email"
-                name="email" 
-                data-testid="common_register__input-email" 
-                onChange={ ( { e }) => setEmail(e.target.value) }
-                />
-            </label>
-            <label>
-                Senha:
-                <input 
-                type="password" 
-                name="senha" 
-                data-testid="common_register__input-password" 
-                onChange={ ( { e }) => setPassword(e.target.value) }
-                />
-            </label>
-        <button 
-        data-testid="common_register__button-register" 
-        type="button"
-        disabled={ isDisabled }
-        onClick={ () => handleSubmitRegister() }
+      <form>
 
-        >Cadastrar</button>
-        { errorMsg && <p data-testid="common_login__element-invalid-email" >Erro ao cadastrar</p> }
-        </form>
+        Nome:
+        <input
+          type="text"
+          name="nome"
+          data-testid="common_register__input-name"
+          onChange={ ({ target }) => setName(target.value) }
+        />
+
+        Email:
+        <input
+          type="email"
+          name="email"
+          data-testid="common_register__input-email"
+          onChange={ ({ target }) => setEmail(target.value) }
+        />
+
+        Senha:
+        <input
+          type="password"
+          name="senha"
+          data-testid="common_register__input-password"
+          onChange={ ({ target }) => setPassword(target.value) }
+        />
+
+        <button
+          data-testid="common_register__button-register"
+          type="button"
+          disabled={ isDisabled }
+          onClick={ () => handleSubmitRegister() }
+        >
+          Cadastrar
+        </button>
+        { errorMsg
+        && <p data-testid="common_login__element-invalid-email">Erro ao cadastrar</p> }
+      </form>
     </div>
   );
 }
