f8d3ce5a0a3239b855fd6539b61ccbcd3248393b
Rodrigo <rodrigopython16@gmail.com>
2022-01-05 17:05:37 -0300

    fixing test 5

--- back-end/src/database/seeders/20220103192949-users.js
+++ back-end/src/database/seeders/20220103192949-users.js
@@ -6,20 +6,23 @@ module.exports = {
       "users",
       [
         {
-          name: "Delivery App Admin",
           email: "adm@deliveryapp.com",
+          id: 1,
+          name: "Delivery App Admin",
           password: "a4c86edecc5aee06eff8fdeda69e0d04",
           role: "administrator",
         },
         {
-          name: "Fulana Pereira",
           email: "fulana@deliveryapp.com",
+          id: 2,
+          name: "Fulana Pereira",
           password: "3c28d2b0881bf46457a853e0b07531c6",
           role: "seller",
         },
         {
-          name: "Cliente Zé Birita",
           email: "zebirita@email.com",
+          id: 3,
+          name: "Cliente Zé Birita",
           password: "1c37466c159755ce1fa181bd247cb925",
           role: "customer",
         },
