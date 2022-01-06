3b60928d51c84a4352b0be3daea755b41481225d
Rodrigo <rodrigopython16@gmail.com>
2022-01-05 23:03:23 -0300

    fix token with fs

--- back-end/src/api/services/userService.js
+++ back-end/src/api/services/userService.js
@@ -1,43 +1,35 @@
 const md5 = require('md5');
-// const jwt = require('jsonwebtoken');
-// const fs = require('fs');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
 const User = require('../../database/models').user;
 
-// const createToken = async (name, email) => {
-//   const key = fs.readFileSync('../../jwt.evaluation.key');
-//   // console.log(key);
-//   // jwt.sign({ user: { name, email } }, key);
-//   return key;
-// }
-
-const hashPassword = (password) => md5(password);
+const createToken = async (name, email) => {
+  const key = fs.readFileSync('./jwt.evaluation.key', 'utf-8');
+  const token = jwt.sign({ user: { name, email } }, key);
+  return token;
+}
 
 const login = async ({ email, password }) => {
     const user = await User.findOne({ where: { email, password: md5(password) } });
 
-    if (!user) {
-      return { error: 'Usuário ou senha inválidos' };
-    }
+    if (!user) throw new Error('Usuário ou senha inválidos');
 
-    // const token = createToken(user.name, email);
+    const token = createToken(user.name, email);
 
-    return user;
+    return token;
 };
 
 const register = async ({ email, name, password }) => {
     const userEmail = await User.findOne({ where: { email } });
     const userName = await User.findOne({ where: { name } });
 
-    if (userEmail || userName) {
-      return { error: 'Usuário cadastrado' };
-    }
+    if (userEmail || userName) throw new Error('Usuário cadastrado');
   
-    const hashedPassword = hashPassword(password);
-    User.create({ email, name, password: hashedPassword, role: 'customer' });
+    User.create({ email, name, password: md5(password), role: 'customer' });
   
-    // const token = createToken(name, email);
-    return { email, name };
+    const token = createToken(name, email);
+    return token;
 };
 
 module.exports = {
