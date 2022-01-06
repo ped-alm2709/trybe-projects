d78f1c6e2d8057e929ea74cd7a57862fb8ee2c04
Rodrigo <rodrigopython16@gmail.com>
2022-01-06 20:38:28 -0300

    added req 15

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
+app.use('/images', express.static(path.join(__dirname, '..', '..', 'public')));
 app.use(express.json());
 app.use(cors());
 
