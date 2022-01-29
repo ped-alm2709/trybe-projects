610c153c879a7160a101c9b60f21267339608ee2
Pedro de Almeida <ped.alm2709@gmail.com>
2022-01-29 14:08:14 -0300

    começo da implementação do test_sorting

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,20 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
 
 
 def test_sort_by_criteria():
-    pass
+    jobs = [
+        {"max_salary": 1},
+        {"max_salary": 10},
+        {"max_salary": 30},
+        {"max_salary": 20},
+        {"max_salary": 5},
+    ]
+
+    sort_by(jobs, "max_salary")
+    assert jobs == [
+        {"max_salary": 1},
+        {"max_salary": 5},
+        {"max_salary": 10},
+        {"max_salary": 20},
+        {"max_salary": 30},
+    ]
