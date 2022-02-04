26c701bd4aa3017a8a5c82029a90b96e93f70dbb
Thiago Leite <leite.tjs@gmail.com>
2022-02-04 20:02:22 -0300

    feat: req3, req4, req5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,17 +1,37 @@
 import csv
+import json
+import xmltodict
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
 
 class Inventory:
+
     @classmethod
     def read_file(cls, path):
-        reports = []
-        with open(path) as file:
-            reports_list = csv.DictReader(file, delimiter=',', quotechar='"')
-            for report in reports_list:
-                reports.append(report)
-        return reports
+        extension = path.split('.')[1]
+        print(extension)
+        if extension == 'csv':
+            data = []
+            with open(path) as file:
+                reports_list = csv.DictReader(
+                    file, delimiter=',', quotechar='"')
+                for report in reports_list:
+                    data.append(report)
+                return data
+        elif extension == 'json':
+            with open(path) as file:
+                reports_list = json.load(file)
+                data = reports_list
+            return data
+        elif extension == 'xml':
+            data = []
+            with open(path, 'r', encoding='utf-8') as file:
+                my_xml = file.read()
+                teste = xmltodict.parse(my_xml)
+                for d in teste['dataset']['record']:
+                    data.append(d)
+            return data
 
     @classmethod
     def import_data(cls, path, report_type):
