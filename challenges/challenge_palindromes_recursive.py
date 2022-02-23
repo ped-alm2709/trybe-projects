8c1faaae4706ea7e93e43bbf8a2ab9a6fb439ca2
Pedro de Almeida <ped.alm2709@gmail.com>
2022-02-23 10:24:41 -0300

    tentativa req 2

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,2 +1,14 @@
+# low_index = 0 e high_index = len(word - 1)
+
+
 def is_palindrome_recursive(word, low_index, high_index):
-    """ Faça o código aqui. """
+    if type(word) is not str or word == "":
+        return False
+
+    if len(word) < 2:
+        return True
+
+    if word[low_index] == word[high_index]:
+        return is_palindrome_recursive(word, (low_index + 1), (high_index - 1))
+    else:
+        return False
