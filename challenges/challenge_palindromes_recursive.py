ce2943500c4c39a3b36ca1019828199e933b543d
Pedro de Almeida <ped.alm2709@gmail.com>
2022-02-23 10:37:39 -0300

    req 2 segunda tentativa

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -5,7 +5,7 @@ def is_palindrome_recursive(word, low_index, high_index):
     if type(word) is not str or word == "":
         return False
 
-    if len(word) < 2:
+    if len(word) < 2 or (low_index, high_index == 0):
         return True
 
     if word[low_index] == word[high_index]:
