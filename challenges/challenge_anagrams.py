ca3cddc0dfadc18fdbfb9761c1f1da4d70c87166
Pedro de Almeida <ped.alm2709@gmail.com>
2022-02-23 15:58:23 -0300

    tentativa req 3

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,27 @@
+# Anagramas em Python
+# https://programacaocompython.blogspot.com/2010/01/anagramas.html
+
+# KeyErrors
+# https://docs.python.org/3/library/exceptions.html#KeyError
+
+
+def characters_counter(string):
+    dict_counter = dict()
+
+    for letter in string:
+        try:
+            dict_counter[letter] += 1
+        except KeyError:
+            dict_counter[letter] = 1
+
+    return dict_counter
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    characters_counter(first_string)
+    characters_counter(second_string)
+
+    if characters_counter(first_string) == characters_counter(second_string):
+        return True
+    else:
+        return False
