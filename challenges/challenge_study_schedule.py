e67d668305e2436416b05f5e9da9690de10f460e
Pedro de Almeida <ped.alm2709@gmail.com>
2022-02-23 09:51:17 -0300

    teste req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -5,3 +5,13 @@
 def study_schedule(permanence_period, target_time):
     if not target_time:
         return None
+
+    counter = 0
+
+    for entrance, exit in permanence_period:
+        if type(entrance) is not int or type(exit) is not int:
+            return None
+        if entrance <= target_time <= exit:
+            counter += 1
+
+    return counter
