1246dd7be4c939cbde50d0ef0b49af7a22486395
Pedro de Almeida <ped.alm2709@gmail.com>
2022-03-11 12:46:02 -0300

    desisti de utilizar importação de classe

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,95 +1,69 @@
+import csv
 # defaultdict:
 # https://docs.python.org/3/library/collections.html#collections.defaultdict
+from collections import defaultdict
 
-
-import csv
-
-
-class AnalyzeLog():
-    def __init__(self):
-        self.orders_report = dict()
-        self.days_in_operation = dict()
-        self.total_orders = dict()
-        self.menu = set()
-
-    def adding_log(self, customer, order, day):
-        self.menu.add(order)
-        if customer not in self.orders_report:
-            self.orders_report[customer] = [(order, day)]
-        else:
-            self.orders_report[customer].append((order, day))
-
-        if order not in self.total_orders:
-            self.total_orders[order] = 1
-        else:
-            self.total_orders[order] += 1
-
-        if day not in self.days_in_operation:
-            self.days_in_operation[day] = 1
-        else:
-            self.days_in_operation[day] += 1
-
-# Max value dictionary:
-# https://datagy.io/python-get-dictionary-key-with-max-value/
-
-# Erro de ValueError: I/O operation on closed file.
-# https://pt.stackoverflow.com/questions/364156/erro-de-valueerror-i-o-operation-on-closed-file-ao-gerar-csv-com-python
-    def most_requested_dish(self, customer):
-        customer_log = dict()
-        for order, _ in self.orders_report[customer]:
-            if order not in customer_log:
-                customer_log[order] = 1
-            else:
-                customer_log[order] += 1
-        return max(customer_log, key=customer_log.get)
-
-    def amount_of_preferred_dish(self, customer, dish):
-        customer_log = dict()
-        for order, _ in self.orders_report[customer]:
-            if order not in customer_log:
-                customer_log[order] = 1
-            else:
-                customer_log[order] += 1
-        return customer_log[dish]
-
-    def never_requested_dish(self, customer):
-        dishes_log = set()
-        for order, _ in self.orders_report[customer]:
-            dishes_log.add(order)
-        return self.menu.difference(dishes_log)
-
-    def days_never_visited(self, customer):
-        opened_days = set(day for day in self.days_in_operation.keys())
-        visited_days = set()
-        for _, day in self.orders_report[customer]:
-            visited_days.add(day)
-        return opened_days.difference(visited_days)
-
-    def busiest_day(self):
-        return max(self.days_in_operation, key=self.days_in_operation.get)
-
-    def least_busy_day(self):
-        return min(self.days_in_operation, key=self.days_in_operation.get)
-
-
-def analyze_log(path_to_file):
-    analyze_class = AnalyzeLog()
 
 # Reading CSV file:
 # https://www.geeksforgeeks.org/reading-csv-files-in-python/
-    with open(path_to_file) as file_csv:
-        data_log = csv.reader(file_csv)
-        for customer, order, day in data_log:
-            analyze_class.adding_log(customer, order, day)
+def csv_reader(path_file):
+    with open(path_file, mode='r') as csv_file:
+        csv_report = csv.reader(csv_file)
+        return list(csv_report)
 
-    data_report = [
-        f"{analyze_class.most_requested_dish('maria')}\n",
-        f"{analyze_class.amount_of_preferred_dish('arnaldo', 'hamburguer')}\n",
-        f"{analyze_class.never_requested_dish('joao')}\n",
-        f"{analyze_class.days_never_visited('joao')}\n"
-    ]
 
-# Writing TXT file:
-# https://www.geeksforgeeks.org/reading-writing-text-files-python/
-    with open('./data/mkt_campaign.txt', 'w') as file_txt:
-        file_txt.writelines(data_report)
+# Max value dictionary:
+# https://datagy.io/python-get-dictionary-key-with-max-value/
+def most_requested_dish(report, customer):
+    most_requested_dish = defaultdict(int)
+    for name, item, _ in report:
+        if name == customer:
+            most_requested_dish[item] += 1
+    max_amount = max(most_requested_dish, key=most_requested_dish.get)
+    return max_amount
+
+
+def hamburguers_amount(report):
+    quantity_of_hamburgers = 0
+    for name, item, _ in report:
+        if name == "arnaldo" and item == "hamburguer":
+            quantity_of_hamburgers += 1
+    return str(quantity_of_hamburgers)
+
+
+def never_requested_dish(report, customer):
+    orders = set()
+    orders_by_customer = set()
+    for name, item, _ in report:
+        if name == customer:
+            orders_by_customer.add(item)
+        orders.add(item)
+    return orders.difference(orders_by_customer)
+
+
+def days_never_visited(report, customer):
+    days = set()
+    customer_days = set()
+    for name, _, day in report:
+        if name == customer:
+            customer_days.add(day)
+        days.add(day)
+    return days.difference(customer_days)
+
+
+def analyze_log(path_to_file):
+    try:
+        report = csv_reader(path_to_file)
+    except FileNotFoundError:
+        error = f"No such file or directory: '{path_to_file}'"
+        raise FileNotFoundError(error)
+    else:
+        # Writing TXT file:
+        # https://www.geeksforgeeks.org/reading-writing-text-files-python/
+        with open("data/mkt_campaign.txt", mode="w") as file:
+            file.write(
+                f"{most_requested_dish(report, 'maria')}\n"
+                f"{hamburguers_amount(report)}\n"
+                f"{never_requested_dish(report, 'joao')}\n"
+                f"{days_never_visited(report, 'joao')}\n"
+            )
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,27 +1,44 @@
-from src.analyze_log import AnalyzeLog
+from src.analyze_log import (
+    most_requested_dish,
+    never_requested_dish,
+    days_never_visited,
+)
+
+from collections import defaultdict
+
+
+def visited_days(log):
+    days = defaultdict(int)
+    for _, _, day in log:
+        days[day] += 1
+    return days
 
 
 class TrackOrders:
     def __init__(self):
-        self.analyze_log = AnalyzeLog()
+        self._orders = list()
 
     def __len__(self):
-        return len(self.analyze_log.total_orders)
+        return len(self._orders)
 
     def add_new_order(self, customer, order, day):
-        self.analyze_log.adding_log(customer, order, day)
+        self._orders([customer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, customer):
-        self.analyze_log.most_requested_dish(customer)
+        most_requested_dish(customer)
 
     def get_never_ordered_per_costumer(self, customer):
-        self.analyze_log.never_requested_dish(customer)
+        never_requested_dish(customer)
 
     def get_days_never_visited_per_costumer(self, customer):
-        self.analyze_log.days_never_visited(customer)
+        days_never_visited(customer)
 
     def get_busiest_day(self):
-        self.analyze_log.busiest_day()
+        days = visited_days(self._orders)
+        max_item = max(days, key=days.get)
+        return max_item
 
     def get_least_busy_day(self):
-        self.analyze_log.least_busy_day()
+        days = visited_days(self._orders)
+        min_item = min(days, key=days.get)
+        return min_item
