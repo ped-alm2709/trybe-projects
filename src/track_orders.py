5c107be71e2c2b671cfadaf3588e5b739f688da9
Pedro de Almeida <ped.alm2709@gmail.com>
2022-03-11 15:45:05 -0300

    requisito 2 rodando localmente

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,9 +1,3 @@
-from src.analyze_log import (
-    most_requested_dish,
-    never_requested_dish,
-    days_never_visited,
-)
-
 from collections import defaultdict
 
 
@@ -16,29 +10,49 @@ def visited_days(log):
 
 class TrackOrders:
     def __init__(self):
-        self._orders = list()
+        self.filtered_list = []
 
     def __len__(self):
-        return len(self._orders)
+        return len(self.filtered_list)
 
     def add_new_order(self, customer, order, day):
-        self._orders([customer, order, day])
+        self.filtered_list.append([customer, order, day])
 
     def get_most_ordered_dish_per_costumer(self, customer):
-        most_requested_dish(customer)
+        result = dict()
+        for customer, meal, _ in self.filtered_list:
+            result[customer] = meal
+
+        return result[customer]
 
     def get_never_ordered_per_costumer(self, customer):
-        never_requested_dish(customer)
+        result = dict()
+        dishes = set()
+        for customer, meal, _ in self.filtered_list:
+            dishes.add(meal)
+        if customer not in result:
+            result[customer] = set()
+        result[customer].add(meal)
+
+        return dishes - result[customer]
 
     def get_days_never_visited_per_costumer(self, customer):
-        days_never_visited(customer)
+        result = dict()
+        days = set()
+        for customer, _, day in self.filtered_list:
+            days.add(day)
+            if customer not in result:
+                result[customer] = set()
+            result[customer].add(day)
+
+        return days - result[customer]
 
     def get_busiest_day(self):
-        days = visited_days(self._orders)
+        days = visited_days(self.filtered_list)
         max_item = max(days, key=days.get)
         return max_item
 
     def get_least_busy_day(self):
-        days = visited_days(self._orders)
+        days = visited_days(self.filtered_list)
         min_item = min(days, key=days.get)
         return min_item
