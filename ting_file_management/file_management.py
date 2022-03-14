491de33d1ff3dd02fbf84f81d61a8758319bf480
Pedro de Almeida <ped.alm2709@gmail.com>
2022-03-14 10:48:19 -0300

    req 2 rodando localmente

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,19 @@
+# sys module:
+# https://pt.stackoverflow.com/questions/432235/como-funciona-o-m%C3%B3dulo-sys-do-python-e-para-que-ele-serve
+# https://docs.python.org/3/library/sys.html
+
+# How to print to stderr in Python
+# https://stackoverflow.com/questions/5574702/how-to-print-to-stderr-in-python
+# http://www.bosontreinamentos.com.br/programacao-em-python/guia-basico-da-funcao-print-em-python/
+
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith('.txt'):
+        sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file, 'r') as txt_file:
+            return txt_file.read().split('\n')
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
